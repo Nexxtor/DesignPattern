d8f1861e05d1fd43c9f0f6ebbd3f3d6b3a492619
Nestor Aldana <nestor.aldana1@gmail.com>
2018-05-13 16:34:43 -0600

    If to Switch

--- src/patterns/creational/factory/dogs/DogFactory.java
+++ src/patterns/creational/factory/dogs/DogFactory.java
@@ -2,13 +2,14 @@ package patterns.creational.factory.dogs;
 
 public class DogFactory {
     public static Dog getDog(String criteria) {
-        if (criteria.equals("small"))
-            return new Poodle();
-        else if (criteria.equals("big"))
-            return new Rottweiler();
-        else if (criteria.equals("working"))
-            return new SiberianHusky();
-
+        switch (criteria) {
+            case "small":
+                return new Poodle();
+            case "big":
+                return new Rottweiler();
+            case "working":
+                return new SiberianHusky();
+        }
         return null;
     }
 }
