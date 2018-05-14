cddb27f9f5b2db56d194f87669294dfbfed6a2ae
Nestor Aldana <nestor.aldana1@gmail.com>
2018-05-14 07:54:12 -0600

    Add Override Annotation in speak method

--- src/patterns/creational/factory/dogs/Rottweiler.java
+++ src/patterns/creational/factory/dogs/Rottweiler.java
@@ -1,6 +1,7 @@
 package patterns.creational.factory.dogs;
 
 class Rottweiler implements Dog {
+    @Override
     public void speak() {
         System.out.println("The Rottweiler says (in a very deep voice) \"WOOF!\"");
     }
--- src/patterns/creational/factory/dogs/SiberianHusky.java
+++ src/patterns/creational/factory/dogs/SiberianHusky.java
@@ -1,6 +1,7 @@
 package patterns.creational.factory.dogs;
 
 class SiberianHusky implements Dog {
+   @Override
     public void speak() {
         System.out.println("The husky says \"Dude, what's up?\"");
     }
